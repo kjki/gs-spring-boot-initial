26e973ba6006510107e6f3685418cfbb14599281
kjki <kjki@mz.co.kr>
2021-11-19 14:46:25 +0900

    deploy script edit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,17 @@
 DEPLOY_PATH=$(ls /tmp/*.jar)
 echo "deploy_path: $DEPLOY_PATH"
 JAR_NAME=$(basename $DEPLOY_PATH)
-echo "> build file name : $JAR_NAME"
+echo "> build 파일명 : $JAR_NAME"
 
-echo "> gs-spring-boot-initial-0.0.1-SNAPSHOT.jar"
+echo "> gs-spring-boot-initial-0.0.1-SNAPSHOT.jar 교체"
 APPLICATION_HOME=/home/ec2-user/
 APPLICATION_JAR_NAME=gs-spring-boot-initial-0.0.1-SNAPSHOT.jar
 APPLICATION_JAR=$APPLICATION_HOME$APPLICATION_JAR_NAME
 
 cp $DEPLOY_PATH $APPLICATION_JAR
 
-echo "> application stop"
+echo "> 실행중인 어플리케이션이 존재하는 경우 중지"
 pgrep -f $APPLICATION_JAR_NAME | xargs kill
 
-echo "> $APPLICATION_JAR deploy"
+echo "> $APPLICATION_JAR 배포"
 sudo -u ec2-user nohup java -jar $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
