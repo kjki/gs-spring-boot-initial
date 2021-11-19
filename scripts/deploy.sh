ceea66a6fd67804b44b35eed721af23346c36d6e
kjki <86279945+kjki@users.noreply.github.com>
2021-11-19 13:14:54 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,14 @@ echo "deploy_path: $DEPLOY_PATH"
 JAR_NAME=$(basename $DEPLOY_PATH)
 echo "> build file name : $JAR_NAME"
 
-echo "> gs-spring-boot-initial-0.0.1-SNAPSHOT.jar Changed.."
+echo "> gs-spring-boot-initial-0.0.1-SNAPSHOT.jar"
 APPLICATION_HOME=/home/ec2-user/
 APPLICATION_JAR_NAME=gs-spring-boot-initial-0.0.1-SNAPSHOT.jar
 APPLICATION_JAR=$APPLICATION_HOME$APPLICATION_JAR_NAME
 
 cp $DEPLOY_PATH $APPLICATION_JAR
 
-echo "> stop application if running.."
+echo "> application stop"
 pgrep -f $APPLICATION_JAR_NAME | xargs kill
 
 echo "> $APPLICATION_JAR deploy"
